68c0c0188508307d94519ce82cfbddf65018cc8b
Admin <solankikishan149@gmail.com>
2025-05-06 16:34:42 +0530

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/work/', // Make sure the base is set to your GitHub repository name
 })
